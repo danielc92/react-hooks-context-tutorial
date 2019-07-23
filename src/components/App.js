7e71fd94a633c260ddd3ccf1fe3b70eb678585de
Daniel Corcoran <danielc.92@outlook.com>
2019-07-23 11:00:39 +1000

    Created navbar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import Navbar from './Navbar';
 
 export default class App extends Component {
     render() {
         return (
             <div>
-                app
+                <Navbar/>
             </div>
         )
     }
