622613e1a159c4c0aef5eed9f6d3dfb02b6317a5
Daniel Corcoran <danielc.92@outlook.com>
2019-07-23 12:23:55 +1000

    Filled in song component with songs and setSongs

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function SongList() {
+
+    const [songs, setSongs] = useState([
+        {title: 'almost home', id: 1},
+        {title: 'memory gospel', id: 2},
+        {title:'this wild darkness', id: 3}
+    ])
+
+    const addSong = () => {
+        let newSong = { title: 'new song', id: 4 }
+        setSongs([...songs, newSong])
+    }
+
     return (
-        <div>
-            song list
+        <div className="SongList">
+            <ul className="collection">
+                {
+                    songs.map(song => 
+                    <li className="collection-item" key={song.id}>{ song.title }</li>)
+                }
+            </ul>
+            <button className="btn" onClick={addSong}>Add a new song</button>
         </div>
     )
 }
