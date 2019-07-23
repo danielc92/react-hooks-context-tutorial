115b8341a4c042b2077cc7e6a846692b29bf731a
Daniel Corcoran <danielc.92@outlook.com>
2019-07-23 11:43:05 +1000

    Accessed contect from BookList

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
 
 export default class BookList extends Component {
+    
+    static contextType = ThemeContext;
+    
     render() {
+        const { isLightTheme, light, dark } = this.context;
+        const theme = isLightTheme ? light : dark; 
         return (
-            <div className="book-list">
-                <li>The way of kings</li>
-                <li>The final empire</li>
-                <li>Gone with the wind</li>
+            <div className="book-list" style={{
+                color: theme.syntax,
+                background: theme.bg
+            }}>
+                <li style={{background: theme.bg}}>The way of kings</li>
+                <li style={{background: theme.bg}}>The final empire</li>
+                <li style={{background: theme.bg}}>Gone with the wind</li>
             </div>
         )
     }
