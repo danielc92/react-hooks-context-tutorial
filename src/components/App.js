171f9b2d4b9a8116c3f0026e3301e2ad49f0e0ec
Daniel Corcoran <danielc.92@outlook.com>
2019-07-23 11:02:06 +1000

    Created Booklist component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import Navbar from './Navbar';
+import BookList from './BookList';
 
 export default class App extends Component {
     render() {
         return (
             <div>
                 <Navbar/>
+                <BookList/>
             </div>
         )
     }
---
+++ src/components/BookList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class BookList extends Component {
+    render() {
+        return (
+            <div className="book-list">
+                <li>The way of kings</li>
+                <li>The final empire</li>
+                <li>Gone with the wind</li>
+            </div>
+        )
+    }
+}
